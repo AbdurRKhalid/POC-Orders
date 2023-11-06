7ce7a0bc0ad170fb19f46698311706ccce176888
Abdur Rehman Khalid <akhalid@sandsmedia.com>
2023-11-06 20:47:44 +0100

    Added the order schema to work further.

---
+++ src/schemas/order.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export type OrderDocument = Order & Document;
+
+@Schema({
+    timestamps: { createdAt: 'created' }
+})
+
+export class Order {
+    @Prop({ required: true })
+    customerId: string;
+
+    @Prop({ required: true })
+    products: {
+        productId: string;
+        amount: string;
+    }
+}
