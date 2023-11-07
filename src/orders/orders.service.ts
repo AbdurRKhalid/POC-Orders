602d54e89cc1795667ead13434ba30c7644d2974
Abdur Rehman Khalid <akhalid@sandsmedia.com>
2023-11-07 21:05:28 +0100

    Add service method to add order to database.

---
+++ src/orders/orders.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { AddOrderDto } from "src/dto/add-order.dto";
+import { Order, OrderDocument } from "src/schemas/order.schema";
+
+@Injectable()
+
+export class OrderService {
+    constructor(@InjectModel(Order.name) private readonly orderModel: Model<OrderDocument>) {}
+
+    addOrder(addOrderDto: AddOrderDto): Promise<OrderDocument> {
+        const order = new this.orderModel(addOrderDto);
+        return order.save();
+    }
+}
